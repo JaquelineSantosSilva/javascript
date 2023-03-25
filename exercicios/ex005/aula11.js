5b065bf27f81b3c1cf552a7ec77740a2e215d47d
Jaqueline dos Santos Silva <126688739+JaquelineSantosSilva@users.noreply.github.com>
2023-03-25 17:01:03 -0300

    exercício proposto aula 11

--- exercicios/ex005/aula11.js
+++ exercicios/ex005/aula11.js
@@ -1,4 +1,7 @@
-var vel = 60.5
+var vel = 58
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel >= 60)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida, Multado`)
+} else {
 console.log('Dirija sempre usando cinto de segurança')
+}
