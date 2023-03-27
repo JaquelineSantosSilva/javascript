ac550b1614d4fcba337022bf5f0be00e01cb5eda
Jaqueline dos Santos Silva <126688739+JaquelineSantosSilva@users.noreply.github.com>
2023-03-27 10:32:40 -0300

    exercício do dia

--- exercicios/ex007/script.js
+++ exercicios/ex007/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('imagem')
+  var agora = new Date()
+  //var hora = agora.getHours()//
+  var hora = 10
+  msg.innerHTML = `Agora são ${hora} horas.`
+ function carregar() {
+    if (hora >=0 && hora < 12) {
+        img.src = 'manha-jpg.jpg'
+        document.body.style.background = '#7979e6'
+    } else if (hora >=12 && hora <= 18){
+        img.src = 'tarde-1.jpg'
+        document.body.style.background = '#e7885c'
+    } else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#504140'
+    }
+ }
+
