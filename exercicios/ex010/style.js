0e43a10340605b46bc315a8d42f007c863e02970
Jaqueline dos Santos Silva <126688739+JaquelineSantosSilva@users.noreply.github.com>
2023-03-30 22:01:38 -0300

    exercício proposto

--- exercicios/ex010/style.js
+++ exercicios/ex010/style.js
@@ -5,18 +5,28 @@ function contar() {
     let res = window.document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
         window.alert('[ERRO] Faltam dados!')
+        
     } else {
-        res.innerHTML= 'Contando'
+        res.innerHTML= 'Contando:   <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let cont = Number(contade.value)
-       for( let c = i; c <= f; c += cont){
-        res.innerHTML += `${c}`
-       }
-    }
-        
-   
-
-
+        if (cont <= 0){
+            window.alert(' contador inválido! considerando o contardor como  1')
+            cont = 1
+        }
+        if (i < f) {
+            //contagem crecsente
+           for( let c = i; c <= f; c += cont){
+             res.innerHTML += `${c} \u{1F449}`
+            } 
+        } else {
+            //contagem decrescente
+            for( let c = i; c >= f; c -= cont) {
+                res.innerHTML += `${c} \u{1F449}`
+               } 
+            }
+      }
 }
