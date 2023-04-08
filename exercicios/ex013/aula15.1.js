d193e997f62c2b593bbdc49b6948a6f8b45bea46
Jaqueline dos Santos Silva <126688739+JaquelineSantosSilva@users.noreply.github.com>
2023-04-08 20:56:52 -0300

    inicio

--- exercicios/ex013/aula15.1.js
+++ exercicios/ex013/aula15.1.js
@@ -1,2 +1,9 @@
 let valores = [1, 2, 4, 5, 8]
 console.log(valores)
+
+//for(let pos = 0; pos < valores.length; pos++) {
+    //console.log(` a posição ${pos} tem o //valor ${valores[pos]}`//
+//}//
+for(let pos in valores) {
+
+}console.log(``)
