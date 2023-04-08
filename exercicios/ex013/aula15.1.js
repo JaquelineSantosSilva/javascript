f7186258441e5c8a1248b1c83989b2f4ff8f91a8
Jaqueline dos Santos Silva <126688739+JaquelineSantosSilva@users.noreply.github.com>
2023-04-08 20:44:37 -0300

    exercícios aula15

---
+++ exercicios/ex013/aula15.1.js
@@ -0,0 +1,2 @@
+let valores = [1, 2, 4, 5, 8]
+console.log(valores)
--- exercicios/ex013/aula15.js
+++ exercicios/ex013/aula15.js
@@ -1,3 +1,7 @@
-let num = [5, 8, 9 ]
+let num = [5, 8, 9, 10, 15 ]
 
-console.log [num]
+num.sort()
+num.push(1)
+console.log(num)
+console.log( `o vetor tem ${num.length} posiçoes`)
+console.log( `o primeiro vetor ${num[0]}`)
