566af56bdc2f401a2a00037dc6cc0522a58f2c80
Jaqueline dos Santos Silva <126688739+JaquelineSantosSilva@users.noreply.github.com>
2023-04-08 21:10:32 -0300

    novo exercicio

--- exercicios/ex013/aula15.1.js
+++ exercicios/ex013/aula15.1.js
@@ -4,6 +4,8 @@ console.log(valores)
 //for(let pos = 0; pos < valores.length; pos++) {
     //console.log(` a posição ${pos} tem o //valor ${valores[pos]}`//
 //}//
-for(let pos in valores) {
+//for(let pos in valores) {
 
-}console.log(``)
+//}console.log(`a posiçao ${pos} tem o valor ${valores[pos]}
+//)//
+Number.inde(2)
